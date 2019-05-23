a8d422a3373ac3bc030ba06173acd5ce7d7dde5b
Elicia Au Duong <eliciaauduong@outlook.com>
2019-05-24 09:51:46 +1000

    added amazalgo practice a

---
+++ amazalgo-practice-a.c
@@ -0,0 +1,17 @@
+// Amazalgo Uni 2019 Practice Contest
+// Given two integers (a and b), get their sum: a?+?b
+
+#include <stdio.h>
+
+int main(void) {
+
+    int num1 = 0;
+    int num2 = 0;
+    scanf("%d%d", &num1, &num2);
+
+    int sum = 0;
+    sum = num1 + num2;
+    printf("%d\n", sum);
+    
+    return 0;
+}
